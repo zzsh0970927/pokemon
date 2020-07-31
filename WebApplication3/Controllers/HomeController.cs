8f945d818991d1b50fcdde9588c7c44d5c0472c4
Kevin Hsu <zzsh0970927@gmail.com>
2020-07-31 22:26:30 +0800

    first

---
+++ WebApplication3/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult mis2000lab()
+        {
+            return View();
+        }
+
+        public ActionResult index1C()
+        {
+            return Redirect("https://yahoo.com.tw") ;
+        }
+
+        protected override void HandleUnknownAction(string actionName)
+        {
+            Response.Redirect("https://yahoo.com.tw");
+            base.HandleUnknownAction(actionName);
+        }
+        
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ WebApplication3/Controllers/MVCController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication3.Controllers
+{
+    public class MVCController : Controller
+    {
+        // GET: MVC
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult index1A()
+        {
+            return View("IndexHello");
+        }
+    }
+}
